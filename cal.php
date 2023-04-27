f480a617161a08c85930e5c602e9a9f94abccc04
chetaniitbhilai <chetan@iitbhilai.ac.in>
2023-04-27 12:47:19 +0530

    COMMIT3

---
+++ cal.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
+  <body>
+
+        <style>
+        table, th, td {
+          border:1px solid black;
+        }
+        .class{
+          margin: 0 100px 0 100px;
+        }
+        /* tbody > tr :second-of-type {
+          text-align: right;
+          background-color: rgb(225, 229, 244);
+        } */
+        </style>
+        <br><br>
+        <center><h2>LECTURE NOTES</h2></center>
+        <table style="width:80%" class="class">
+
+          <tr>
+          <center>
+            <th>Serial No</th>
+            <th>Topic</th>
+          </center>
+          </tr>
+          <tr>
+            <td>1</td>
+            <td><a href="\www\project\Lecture_Note_1.pdf" target="_blank">Lecture1</a></td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td><a href="\www\project\Lecture_Note_2.pdf" target="_blank">Lecture2</a></td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td><a href="\www\project\Lecture_Note_3.pdf" target="_blank">Lecture3</a></td>
+          </tr>
+            <td>4</td>
+
+            <td><a href="\www\project\Lecture_Note_4.pdf" target="_blank">Lecture4</a></td>
+          </tr>
+          <tr>
+            <td>5</td>
+            <td><a href="\www\project\Lecture_Note_5.pdf" target="_blank">Lecture5</a></td>
+          </tr>
+          <tr>
+            <td>6</td>
+            <td><a href="\www\project\Lecture_Note_6.pdf" target="_blank">Lecture6</a></td>
+          </tr>
+          <tr>
+            <td>7</td>
+            <td><a href="\www\project\Lecture_Note_7.pdf" target="_blank">Lecture7</a></td>
+          </tr>
+          <tr>
+            <td>8</td>
+            <td><a href="\www\project\Lecture_Note_8.pdf" target="_blank">Lecture8</a></td>
+          </tr>
+        </table>
+
+<br><br><br><br><br>
+
+        <center><h2>TUTORIALS</h2></center>
+        <table style="width:80%" class="class">
+
+          <tr>
+          <center>
+            <th>Serial No</th>
+            <th>Topic</th>
+          </center>
+          </tr>
+          <tr>
+            <td>1</td>
+            <td><a href="\www\project\Tutorial_1cal.pdf" target="_blank">Tutorial1</a></td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td><a href="\www\project\Solu_Tutorial_1.pdf" target="_blank">Tutorial1Solutions</a></td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td><a href="\www\project\Tutorial_2cal.pdf" target="_blank">Tutorial2</a></td>
+          </tr>
+          <tr>
+            <td>4</td>
+            <td><a href="\www\project\Solu_Tutorial_2.pdf" target="_blank">Tutorial2Solutions</a></td>
+          </tr>
+          <tr>
+            <td>5</td>
+            <td><a href="\www\project\Tutorial_3cal.pdf" target="_blank">Tutorial3</a></td>
+          </tr>
+          <tr>
+            <td>6</td>
+            <td><a href="\www\project\Solu_Tutorial_3.pdf" target="_blank">Tutorial3Solutions</a></td>
+          </tr>
+          <tr>
+            <td>7</td>
+            <td><a href="\www\project\Tutorial_4cal.pdf" target="_blank">Tutorial4</a></td>
+          </tr>
+          <tr>
+            <td>8</td>
+            <td><a href="\www\project\Solu_Tutorial_4.pdf" target="_blank">Tutorial4Solutions</a></td>
+          </tr>
+          <tr>
+            <td>9</td>
+            <td><a href="\www\project\Tutorial_5cal.pdf" target="_blank">Tutorial5</a></td>
+          </tr>
+          <tr>
+            <td>10</td>
+            <td><a href="\www\project\Solu_Tutorial_5.pdf" target="_blank">Tutorial5Solutions</a></td>
+          </tr>
+
+
+        </table>
+<br><br><br><br><br>
+  </body>
+</html>
---
+++ stt.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
+  <body>
+
+        <style>
+        table, th, td {
+          border:1px solid black;
+        }
+        .class{
+          margin: 0 100px 0 100px;
+        }
+        /* tbody > tr :second-of-type {
+          text-align: right;
+          background-color: rgb(225, 229, 244);
+        } */
+        </style>
+        <br><br>
+        <center><h2>LECTURE NOTES</h2></center>
+        <table style="width:80%" class="class">
+
+          <tr>
+          <center>
+            <th>Serial No</th>
+            <th>Topic</th>
+          </center>
+          </tr>
+          <tr>
+            <td>1</td>
+            <td><a href="\www\project\01-Introduction.pdf" target="_blank">Introduction</a></td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td><a href="\www\project\02-LinuxCommands.pdf" target="_blank">LinuxCommands1</a></td>
+          </tr>
+
+          <tr>
+            <td>3</td>
+            <td><a href="\www\project\03-LinuxCommands2.pdf" target="_blank">LinuxCommands2</a></td>
+          </tr>
+            <td>4</td>
+
+            <td><a href="\www\project\04-LinuxCommands_Scripting.pptx.pdf" target="_blank">Shell Scripting</a></td>
+          </tr>
+          <tr>
+            <td>5</td>
+            <td><a href="\www\project\5_Basics of HTML.pdf" target="_blank">HTML</a></td>
+          </tr>
+          <tr>
+            <td>6</td>
+            <td><a href="\www\project\06-PhP_Basics.pptx(1).pdf" target="_blank">PHP Basics</a></td>
+          </tr>
+          <tr>
+            <td>7</td>
+            <td><a href="\www\project\07-Php_Files_Forms.pptx(1).pdf" target="_blank">Files and Forms</a></td>
+          </tr>
+          <tr>
+            <td>8</td>
+            <td><a href="\www\project\08-GIT.pptx.pdf" target="_blank">GITHUB Basics</a></td>
+          </tr>
+          <tr>
+            <td>9</td>
+            <td><a href="\www\project\9_Latex.pdf" target="_blank">Latex1</a></td>
+          </tr>
+          <tr>
+            <td>10</td>
+            <td><a href="\www\project\10_Latex.pdf" target="_blank">Latex2</a></td>
+          </tr>
+        </table>
+
+<br><br><br><br><br>
+
+        <center><h2>Practice Problems</h2></center>
+        <table style="width:80%" class="class">
+
+          <tr>
+          <center>
+            <th>Serial No</th>
+            <th>Topic</th>
+          </center>
+          </tr>
+          <tr>
+            <td>1</td>
+            <td><a href="\www\project\1_LinuxCommands.pdf" target="_blank">Practice Problems1(LinuxCommands)</a></td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td><a href="\www\project\2_LinuxCommands.pdf" target="_blank">Practice Problems2(LinuxCommands)</a></td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td><a href="\www\project\3_ShellScripting.pdf" target="_blank">Practice Problems3(Shell SCripting)</a></td>
+          </tr>
+          <tr>
+            <td>4</td>
+            <td><a href="\www\project\5_PHP.pdf" target="_blank">Practice Problems4(PHP and HTML)</a></td>
+          </tr>
+          <tr>
+            <td>5</td>
+            <td><a href="\www\project\6_PHP.pdf" target="_blank">Practice Problems5(PHP and HTML)</a></td>
+          </tr>
+          <tr>
+            <td>6</td>
+            <td><a href="\www\project\6_Latex(1).pdf" target="_blank">Practice Problems6(Latex)</a></td>
+          </tr>
+        </table>
+<br><br><br><br><br>
+  </body>
+</html>
